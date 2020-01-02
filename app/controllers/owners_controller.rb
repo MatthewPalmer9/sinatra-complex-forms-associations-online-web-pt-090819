eae551abf384be1e8737d169f0f93aa50efca3be
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-01-02 22:47:22 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,7 +30,7 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    ####### bug fix
+    ####### bug fix required so that 
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
